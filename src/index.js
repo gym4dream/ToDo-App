5d0380e03721eda4294d9fa45834656447b5db6e
Ilya Shestakov <shestakovsailor@gmail.com>
2019-08-04 11:02:59 +0300

    Create React App

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const element = (
+  <div>
+    <h1>My ToDo List</h1>
+    <input type="search" placeholder="search" />
+    <ul>
+      <li>Learn React</li>
+      <li>Create Awesome App</li>
+    </ul>
+  </div>
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(element, document.getElementById('root'));
