5c7a2f97f2914e1d8723be1a23537ca49b840b99
Ilya Shestakov <shestakovsailor@gmail.com>
2019-08-10 16:23:25 +0300

    Added a little css

--- src/components/search.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Search = () => {
-  return <input type="search" placeholder="search" />;
-};
-
-export default Search;
--- src/index.js
+++ src/index.js
@@ -2,23 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import AppHeader from './components/app-header';
-import Search from './components/search';
+import SearchPanel from './components/search-panel';
 import TodoList from './components/todo-list';
+import ItemStatusFilter from './components/item-status-filter';
+
+import './index.css';
 
 const App = () => {
 
   const todoData = [
-    { label: 'Drink Coffee', important: false },
-    { label: 'Create Awesome App', important: true },
-    { label: 'Go Shopping', important: false }
+    { label: 'Drink Coffee', important: false, id: 1 },
+    { label: 'Make Awesome App', important: true, id: 2 },
+    { label: 'Have a lunch', important: false, id: 3 }
   ];
 
   return (
-    <div>
-      <AppHeader />
-      <Search />
+    <div className="todo-app">
+      <AppHeader toDo={1} done={3} />
+      <div className="top-panel d-flex">
+        <SearchPanel />
+        <ItemStatusFilter />
+      </div>
+
       <TodoList todos={todoData} />
     </div>
   );
 };
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<App />,
+  document.getElementById('root'));
